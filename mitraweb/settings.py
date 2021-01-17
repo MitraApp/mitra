ba72ad7e0ea897060333721a6821f882ca9d35f6
Robert <robert@Roberts-MacBook-Air.local>
2021-01-17 15:59:13 -0600

    added deploymnet settings security

--- mitraweb/settings.py
+++ mitraweb/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-rl5e=d4j9*05u3*kmlxfuc6+v%6zdwn38om$wta4&uq$ijou='
+#SECRET_KEY = '-rl5e=d4j9*05u3*kmlxfuc6+v%6zdwn38om$wta4&uq$ijou='
+SECRET_KEY = secrets.token_urlsafe()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -127,3 +129,5 @@ LOGIN_REDIRECT_URL = '/financials'
 LOGOUT_REDIRECT_URL = '/accounts/login'
 
 SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
